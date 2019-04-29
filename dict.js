aa48f1d91b9ed1d55f91e38e2a606c935101ffe1
swamy422@gmail.com <swamy422@gmail.com>
2019-04-29 08:57:52 +0530

    implement word of the day feature

--- dict.js
+++ dict.js
@@ -18,58 +18,83 @@ program
   .parse(process.argv)
 
 //  console.log(cmdValue)
+printProgressDot = () => { process.stdout.write("."); }
+
 if (program.syn) {
+  process.stdout.write("Fetching data from oxforddictionaries API.");
+  var interval = setInterval(printProgressDot, 1000);
   dict.getSynonyms(program.syn)
     .then(response => {
+      clearProcessing(interval)
       Util.fetchSynonyms(response, true)
     })
     .catch(err => {
+      clearProcessing(interval)
       console.log(err)
     })
 }
 else if (program.ant) {
+  process.stdout.write("Fetching data from oxforddictionaries API.");
+  var interval = setInterval(printProgressDot, 1000);
   dict.getAntonyms(program.ant)
     .then(response => {
+      clearProcessing(interval)
       Util.fetchAntonyms(response, true)
     })
     .catch(err => {
+      clearProcessing(interval)
       console.log(err)
     })
 }
 else if (program.def) {
+  process.stdout.write("Fetching data from oxforddictionaries API.");
+  var interval = setInterval(printProgressDot, 1000);
   dict.getDefinitons(program.def)
     .then(response => {
+      clearProcessing(interval)
       Util.fetchDefinitions(response, true)
     })
     .catch(err => {
+      clearProcessing(interval)
       console.log(err)
     })
 }
 else if (program.ex) {
+  process.stdout.write("Fetching data from oxforddictionaries API.");
+  var interval = setInterval(printProgressDot, 1000);
   dict.getExamples(program.ex)
     .then(response => {
+      clearProcessing(interval)
       Util.fetchExamples(response, true)
     })
     .catch(err => {
+      clearProcessing(interval)
       console.log(err)
     })
 }
 else if (program.dict) {
+  process.stdout.write("Fetching data from oxforddictionaries API.");
+  var interval = setInterval(printProgressDot, 1000);
   dict.getFullDictionary(program.dict)
     .then(response => {
+      clearProcessing(interval)
       Util.fetchDefinitions(response.defsAndExs, true)
       Util.fetchSynonyms(response.synonyms, true)
       Util.fetchAntonyms(response.antonyms, true)
       Util.fetchExamples(response.defsAndExs, true)
     })
     .catch(err => {
+      clearProcessing(interval)
       console.log(err)
     })
 }
 else if (program.play) {
   let randWord = randomWords.getRandomWord()
+  process.stdout.write("Fetching data from oxforddictionaries API.");
+  var interval = setInterval(printProgressDot, 1000);
   dict.getFullDictionary(program.dict)
     .then(response => {
+      clearProcessing(interval)
       let data = {}
       var correctWords = []
       data["def"] = Util.fetchDefinitions(response.defsAndExs)
@@ -80,9 +105,36 @@ else if (program.play) {
       Util.playGame( randWord, correctWords, data, false, true, '', '' )
     })
     .catch(err => {
+      clearProcessing(interval)
       console.log(err)
     })
 }
 else {
-  console.log('Wrong command')
+  var args = process.argv.slice(2)
+  if(!args.length) {
+    let wod = randomWords.getWordOfTheDay()
+    process.stdout.write("Fetching data from oxforddictionaries API.");
+    var interval = setInterval(printProgressDot, 1000);
+    dict.getFullDictionary(wod)
+    .then(response => {
+      clearProcessing(interval)
+      Util.fetchDefinitions(response.defsAndExs, true)
+      Util.fetchSynonyms(response.synonyms, true)
+      Util.fetchAntonyms(response.antonyms, true)
+      Util.fetchExamples(response.defsAndExs, true)
+    })
+    .catch(err => {
+      clearProcessing(interval)
+      console.log(err)
+    })
+  }
+  else {
+    console.log('Wrong Input!!')    
+  }
+}
+
+clearProcessing = (handle) => {
+  if(handle) clearInterval(handle)
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
 }
